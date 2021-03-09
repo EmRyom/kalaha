feabc290f88379b1552c3018d6a486f37efb3ed7
EmRyom <61932752+EmRyom@users.noreply.github.com>
2021-03-09 15:11:13 +0100

    Added necessary condition for stealing

--- kalaha4.py
+++ kalaha4.py
@@ -36,15 +36,15 @@ def traverse(board,side,pos):
         board[pos]+=1
         stack-=1
     
-    if board[pos]==1 and (not pos==6) and not pos==13:  # Stealing implimentation
+    if board[pos]==1 and (not pos==6) and (not pos==13) and board[12-pos]>0 :  # Stealing implimentation
         if side and pos in range(0,6):
-            board[6]+=board[12-pos]+1
-            board[12-(pos)]=0
-            board[pos]=0
+                board[6]+=board[12-pos]+1
+                board[12-(pos)]=0
+                board[pos]=0
         if not side and pos in range(7,13):
-            board[13]+=board[12-pos]+1
-            board[12-pos]=0
-            board[pos]=0
+                board[13]+=board[12-pos]+1
+                board[12-pos]=0
+                board[pos]=0
         
     if (side and pos==6) or (not side and pos==13): # Players get another turn 
         return board, side                          # if it ends in their goal
