c06706530c3867074b3ec89c33b06eea25458e71
EmRyom <61932752+EmRyom@users.noreply.github.com>
2021-03-16 09:06:12 +0100

    Update minimax.py

--- minimax.py
+++ minimax.py
@@ -3,6 +3,82 @@ from kalaha import traverse
 
 depth = 4
 
+def terminalTest(board):
+    
+    return (sum(board[0:6])==0 or sum(board[7:13])==0)
+
+
+def utility(board, side):
+    
+    if side:
+        return board[6]-board[13]
+    else:
+        return -board[6]+board[13]
+
+
+def maxValue(board, side, alpha, beta, count, depth): 
+    
+    # limiting depth    
+    if terminalTest(board) or count == depth:
+        return utility(board, side), None
+    
+    v = -100
+    actions = nextMoves(board, side)
+    # dummy statement
+    final_action = actions[0]
+    
+    for a in actions:
+        tboard = a[0][0]
+        tside = a[0][1]
+        v = max(v, minValue(tboard, tside, alpha, beta, count+1, depth))
+        #debug
+        print(str(count) + ':')
+        print('pos: ' + str(a[1]))
+        print('v: ' + str(v))
+        if v >= beta:
+            return v
+        alpha = max(alpha, v)
+        
+        # storing best action
+        if count == 1 and alpha == v:
+            final_action = a
+        
+        
+    return v, final_action
+
+
+def minValue(board, side, alpha, beta, count, depth):
+    
+    # limiting depth    
+    if terminalTest(board) or count == depth:
+        return utility(board, side)
+    
+    v = 100
+    actions = nextMoves(board, side)
+    
+    for a in actions:
+        tboard = a[0][0]
+        tside = a[0][1]
+        v = min(v, maxValue(tboard, tside, alpha, beta, count+1, depth)[0])
+        #debug
+        print(str(count) + ':')
+        print('pos: ' + str(a[1]))
+        print('v: ' + str(v))
+        if v <= alpha:
+            return v
+        beta = min(beta, v)
+    
+    return v
+
+
+def alphaBetaSearch(board, side, depth):
+    
+    final_action = maxValue(board, side, -100, 100, 0, depth)[1]
+    
+    return final_action
+    
+    
+
 def nextMoves(board,side):
     l = []
     for pos in range(1,7):
@@ -21,20 +97,8 @@ def nextMoves(board,side):
 
 
 def judge(board,side):
-    l = nextMoves(board,side)
-    max = -48
-    move = 0
-    for i in l:
-        b = i[0][0]
-        
-        
-        if b[6]-b[13]> max and side:
-            max = b[6]-b[13]
-            move = i[1]
-            
-        if -b[6]+b[13]> max and not side:
-            max = -b[6]+b[13]
-            move = i[1]
+    
+    move = alphaBetaSearch(board, side, 3)[1]
             
     return move
         
