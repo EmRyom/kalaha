218d9b0a4f795e46723494ab73c50abe52f9348a
EmRyom <61932752+EmRyom@users.noreply.github.com>
2021-03-16 16:22:43 +0100

    ½ of tests start with south first

--- evaluation.py
+++ evaluation.py
@@ -13,6 +13,8 @@ def evaluation(n):
             c=4
             board=[c,c,c,c,c,c,0,  c,c,c,c,c,c,0]
             side = True
+            if p>n/2:
+                side = False
             while(True):
                 if sum(board[0:6])==0 or sum(board[7:13])==0:
                     board[6] += sum(board[0:6])
@@ -33,7 +35,19 @@ def evaluation(n):
                     board,side = traverse(board,False,i+6)
             print(w,d,l,depth)
         res.append([w,d,l,depth,time()-start])
-    print(res)
+    for i in res:
+        print(i)
+        
+        
+'''
+wins, draws, losses, depth, time (s)
+[983, 6, 11, 2, 3.424082040786743]
+[982, 6, 12, 3, 11.545182466506958]
+[990, 2, 8, 4, 40.22028565406799]
+[986, 3, 11, 5, 109.28872013092041]
+[997, 1, 2, 6, 322.5370743274689]
+[999, 0, 1, 7, 1015.6422221660614]
+'''
 
         
         
